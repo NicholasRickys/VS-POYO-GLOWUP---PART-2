69c8c7a7352e5f1929c1044c5f480afd7c5f427e
Nicholas Rickys <nicholas.rickys3@gmail.com>
2024-02-11 16:14:54 -0500

    bru

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -13,7 +13,7 @@ import flixel.group.FlxGroup;
 import flixel.input.gamepad.FlxGamepad;
 import flixel.math.FlxPoint;
 import flixel.math.FlxRect;
-import flixel.system.FlxSound;
+import flixel.sound.FlxSound;
 import flixel.system.ui.FlxSoundTray;
 import flixel.text.FlxText;
 import flixel.tweens.FlxEase;
@@ -59,8 +59,10 @@ class TitleState extends MusicBeatState
 		// DEBUG BULLSHIT
 
 		super.create();
-
+		
+		#if newgrounds
 		NGio.noLogin(APIStuff.API);
+		#end
 
 		#if ng
 		var ng:NGio = new NGio(APIStuff.API, APIStuff.EncKey);
